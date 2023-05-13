e5dd2666e44d838ba88da43a940bd7b5df6ccff6
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-05-13 12:29:58 +0530

    commit

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(passedStudents)
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(passedStudents);
+}
+
+function passedStudents(student) {
+  if(student.marks > 50) {
+    console.log(student);
+  }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i].marks < 50) {
+      delete arr[i];
+    }
+  }
+
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i] != undefined) {
+      console.log(arr[i]);
+    }
+  }
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let students = [
+    { id: 10, name: "Rohan", age: "8", marks: 75 },
+    { id: 11, name: "Roshan", age: "12", marks: 95 },
+    { id: 12, name: "Rohit", age: "9", marks: 37 },
+  ];
+  console.log(students);
 }
